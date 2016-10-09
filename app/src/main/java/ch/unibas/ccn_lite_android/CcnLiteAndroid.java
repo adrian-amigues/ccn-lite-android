339770d8181f67e9ec22312b32e5f4d021907e79
arisha <arisha_csd@hotmail.com>
2016-10-09 22:24:51 +0200

    TODO.make logo as separate fragment fix all colors.add sensors and spinner at demo

--- app/src/main/java/ch/unibas/ccn_lite_android/CcnLiteAndroid.java
+++ app/src/main/java/ch/unibas/ccn_lite_android/CcnLiteAndroid.java
@@ -4,21 +4,11 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import android.app.Activity;
-
-import android.content.ComponentName;
-
 import android.app.FragmentManager;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
-import android.os.AsyncTask;
-import android.os.IBinder;
-import android.text.method.ScrollingMovementMethod;
-
 import android.graphics.Color;
 
 import android.app.Fragment;
@@ -27,7 +17,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
-
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -45,21 +34,10 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.PopupMenu.OnMenuItemClickListener;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.AdapterView;
-import java.util.List;
-import java.util.ArrayList;
+
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Toast;
-
-
-import static android.R.attr.port;
-import static ch.unibas.ccn_lite_android.R.id.resultTextView;
-
-
 
 import com.google.android.gms.maps.MapFragment;
 
@@ -67,6 +45,7 @@ import ch.unibas.ccn_lite_android.fragments.DeleteDatabaseFragment;
 import ch.unibas.ccn_lite_android.fragments.HistoryFragment;
 import ch.unibas.ccn_lite_android.fragments.HomeFragment;
 import ch.unibas.ccn_lite_android.fragments.PreferencesFragment;
+import ch.unibas.ccn_lite_android.fragments.SensorsInfo;
 import ch.unibas.ccn_lite_android.fragments.addToDatabase;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
@@ -74,28 +53,19 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListener
 {
-
     ArrayAdapter adapter;
+    String hello;
     Context ccnLiteContext;
+    int newData;
     SQLiteDatabase sensorDatabase;
     String resultValue;
 
-    String ipString; // var for server ip
-    String portString; //port
-    String contentString;//Interest Object Name
+    String ipString;
+    String portString;
+    String contentString;
     private Handler mHandler;
-    Spinner ex;
-
-
-    //    For service
-    RelayService mService;
-    boolean mBound = false;
-
-    TextView resultTextView;
-
 
     private static String TAG = CcnLiteAndroid.class.getSimpleName();
-
 
     ListView mDrawerList;
     RelativeLayout mDrawerPane;
@@ -104,9 +74,6 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
 
     ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
 
-    /**
-     * @desc create new activity and init relay with CCN
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -119,19 +86,12 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
         mNavItems.add(new NavItem("Add", "Add to database", R.drawable.ic_add_black_24dp));
         mNavItems.add(new NavItem("Delete", "Delete from datasbase", R.drawable.ic_delete_black_24dp));
         mNavItems.add(new NavItem("History", "Previous sensor values", R.drawable.ic_history_black_24dp));
-        mNavItems.add(new NavItem("Sensors", "See on GoogleMap", R.drawable.ic_place_black_24dp));
+        mNavItems.add(new NavItem("Sensors Location", "See on GoogleMap", R.drawable.ic_place_black_24dp));
+        mNavItems.add(new NavItem("Sensors Info", " ", R.drawable.ic_settings_input_antenna_black_24dp));
 
 
         // DrawerLayout
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-
-
-        ex = (Spinner) findViewById(R.id.test_example);
-        List<String> list = new ArrayList<String>();
-        list.add("/android/test/mycontent");
-        list.add("/ccn/sensor/tmp");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
-                (this, R.layout.spinner_item,list);
 
         // Populate the Navigtion Drawer with options
         mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
@@ -147,10 +107,18 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
             }
         });
 
-        if(mBound) {
-            mService.startRely();
-        }
+       /* String arraySpinner[] = new String[] {
+                "CCNx2015", "NDN2013", "CCNB", "IOT2014", "LOCALRPC", "LOCALRPC"
+        };
 
+        Spinner s = (Spinner) findViewById(R.id.formatSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+
+        s.setAdapter(adapter);
+*/
+
+        hello = relayInit();
         ccnLiteContext = this;
     }
 
@@ -207,8 +175,8 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
                     .commit();
 
             TextView historyLocation = (TextView) findViewById(R.id.historyShowTextView2);
-           // historyLocation.setMovementMethod(new ScrollingMovementMethod());
-           // historyLocation.setText("Number of Items: " + count + "\n" + sensorValue , TextView.BufferType.EDITABLE);
+            // historyLocation.setMovementMethod(new ScrollingMovementMethod());
+            // historyLocation.setText("Number of Items: " + count + "\n" + sensorValue , TextView.BufferType.EDITABLE);
 
 
             mDrawerList.setItemChecked(position, true);
@@ -216,6 +184,15 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
         }else if(position == 4){//Sensors
             Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
+        }else  if(position == 5){//Home
+            fragment = new SensorsInfo();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.mainContent, fragment)
+                    .commit();
+
+            mDrawerList.setItemChecked(position, true);
+            setTitle(mNavItems.get(position).mTitle);
         }
 
 
@@ -227,31 +204,31 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
 
     @Override
     public void onStart() {
-        super.onStart();
-
         ListView lv;
         sensorDatabase = openOrCreateDatabase("SENSORDATABASE",MODE_PRIVATE,null);
         sensorDatabase.execSQL("CREATE TABLE IF NOT EXISTS sensorTable(sensorValue VARCHAR);");
 
-        // Bind to RelayService
-        Intent intent = new Intent(this, RelayService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-        Toast.makeText(this, "mBound = " + mBound, Toast.LENGTH_SHORT).show();
-
-        Button b = (Button) findViewById(R.id.sendButton);
-
         super.onStart();
-
+       /* Button b = (Button) findViewById(R.id.sendButton);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String text = ex.getSelectedItem().toString();
+                RelativeLayout myLayout = (RelativeLayout) findViewById(R.id.myLayout);
+               // myLayout.setBackgroundColor(Color.rgb(1,0,0));
+                EditText ip = (EditText) findViewById(R.id.IPEditText);
+                ipString = ip.getText().toString();
+                EditText port = (EditText) findViewById(R.id.portEditText);
+                portString = port.getText().toString();
+                int portInt = Integer.parseInt(portString);
+                EditText content = (EditText) findViewById(R.id.contentEditText);
+                contentString = content.getText().toString();
                 mHandler = new Handler();
-                if(text.equals("/android/test/mycontent")) {
-                    new AndroidPeek().execute("130.238.15.221", "9999", text);
-                }
-                else new AndroidPeek().execute("130.238.15.225", "9999", text);
+                resultValue = androidPeek(ipString, portInt, contentString);
+                TextView result = (TextView) findViewById(R.id.resultTextView);
+                result.setMovementMethod(new ScrollingMovementMethod());
+                result.setText(resultValue, TextView.BufferType.EDITABLE);
+
             }
-        });
+        });*/
 
        /* ImageView imageViewMenu = (ImageView) findViewById(R.id.imageViewMenu);
         imageViewMenu.setOnClickListener(new View.OnClickListener(){
@@ -263,19 +240,6 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
     }
 
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Unbind from the service
-        if (mBound) {
-            unbindService(mConnection);
-            mBound = false;
-        }
-    }
-
-
-
-
 
     public void appendToLog(String line) {
         while (adapter.getCount() > 500)
@@ -283,24 +247,6 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
         adapter.add(line);
         adapter.notifyDataSetChanged();
     }
-
-    /** Defines callbacks for service binding, passed to bindService() */
-    private ServiceConnection mConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to RelayService, cast the IBinder and get RelayService instance
-            RelayService.LocalBinder binder = (RelayService.LocalBinder) service;
-            mService = binder.getService();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
 
     public native String relayInit();
 
@@ -313,41 +259,9 @@ public class CcnLiteAndroid extends Activity //implements OnMenuItemClickListene
      */
     static {
         System.loadLibrary("ccn-lite-android");
-    }
 
-    private class AndroidPeek extends AsyncTask<String, Void, String> {
-        /** The system calls this to perform work in a worker thread and
-         * delivers it the parameters given to AsyncTask.execute() */
-        protected String doInBackground(String... params) {
-            String ipString = params[0];
-            int portInt = Integer.parseInt(params[1]);
-            String contentString = params[2];
-            return mService.startAndroidPeek(ipString, portInt, contentString);
-        }
-
-        /** The system calls this to perform work in the UI thread and delivers
-         * the result from doInBackground() */
-        protected void onPostExecute(String result) {
-            resultTextView.setMovementMethod(new ScrollingMovementMethod());
-            resultTextView.append(result);
-        }
     }
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-    /**
-     * @desc insert values into DB by lick button
-     */
-
 }
-
 
 class NavItem {
     String mTitle;
@@ -409,4 +323,3 @@ class DrawerListAdapter extends BaseAdapter {
         return view;
     }
 }
-
--- app/src/main/java/ch/unibas/ccn_lite_android/fragments/SensorsInfo.java
+++ app/src/main/java/ch/unibas/ccn_lite_android/fragments/SensorsInfo.java
@@ -1,7 +1,28 @@
 package ch.unibas.ccn_lite_android.fragments;
 
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ch.unibas.ccn_lite_android.R;
+
 /**
  * Created by eirini on 10/9/16.
  */
-public class SensorsInfo {
+public class SensorsInfo extends Fragment {
+
+    View rootView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.info_sensors, container, false);
+        return rootView;
+    }
 }
