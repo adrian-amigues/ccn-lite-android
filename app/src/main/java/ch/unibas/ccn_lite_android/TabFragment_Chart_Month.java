85070f067279d320a3a3774d6afdc9b19efdc065
Maria <maria.rajabzadehnamaghi.5780@student.uu.se>
2016-11-07 14:59:21 +0100

    change data for Day, add data for Week and Month, change the colors of chart, set legend, set description position, remove rightAxis, add limit line

--- app/src/main/java/ch/unibas/ccn_lite_android/TabFragment_Chart_Month.java
+++ app/src/main/java/ch/unibas/ccn_lite_android/TabFragment_Chart_Month.java
@@ -5,11 +5,9 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.EventLogTags;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Legend;
@@ -26,20 +24,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class TabFragment_Chart extends Fragment {
-
+public class TabFragment_Chart_Month extends Fragment {
 
     public static final String ARG_PAGE = "ARG_PAGE";
 
     private int mPage;
 
-    public static TabFragment_Chart newInstance(int page) {
+
+    public TabFragment_Chart_Month() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static TabFragment_Chart_Month newInstance(int page) {
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, page);
-        TabFragment_Chart fragment = new TabFragment_Chart();
+        TabFragment_Chart_Month fragment = new TabFragment_Chart_Month();
         fragment.setArguments(args);
         return fragment;
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,29 +52,31 @@ public class TabFragment_Chart extends Fragment {
         mPage = getArguments().getInt(ARG_PAGE);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tab_fragment__chart, container, false);
-        BarChart chartTest = (BarChart) view.findViewById(R.id.DayChart);
+        View view = inflater.inflate(R.layout.fragment_tab_fragment__chart__month, container, false);
+        BarChart chartTest = (BarChart) view.findViewById(R.id.MonthChart);
         ViewPortHandler handler = chartTest.getViewPortHandler();
         BarData data = new BarData(getDataSet2());
         data.setBarWidth(0.9f);
         chartTest.setData(data);
 
-        String description = "Noise/Day Chart";
+        String description = "Noise/Month Chart";
         chartTest.setDescription(description);
         chartTest.setDescriptionPosition(handler.contentLeft() + 2*handler.offsetLeft() + 2*description.length(), handler.contentTop());
         chartTest.setDescriptionTextSize(14f);
 
-
         chartTest.setFitBars(true);
         chartTest.setVisibleXRangeMaximum(7);//It makes the chart scrollable
+
         Legend legend = chartTest.getLegend();
         legend.setPosition(Legend.LegendPosition.ABOVE_CHART_RIGHT);
         int[] legendColors = {Color.rgb(67, 189, 1), Color.rgb(254, 161, 2), Color.rgb(253, 1, 1)};
         String[] legendLabels = {"Low", "Medium", "High"};
         legend.setCustom(legendColors, legendLabels);
+
         XAxis xAxis = chartTest.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setTextSize(14f);
@@ -80,9 +87,13 @@ public class TabFragment_Chart extends Fragment {
         xAxis.setGranularity(1f);// minimum axis-step (interval) is 1
         xAxis.setAxisMinValue(0);
         // the labels that should be drawn on the XAxis
-        final String[] quarters = new String[] { "9.00", "10.00", "11.00", "12.00", "13.00", "14.00", "15.00", "16.00", "17.00", "18.00", "19.00", "20.00", "21.00", "22.00", "23.00", "00.00", "00.01" };
+        final String[] quarters = new String[] { "", "5 Oct.", "6 Oct.", "7 Oct.", "8 Oct.", "9 Oct.", "10 Oct.",
+                "11 Oct.", "12 Oct.", "13 Oct.", "14 Oct.", "15 Oct.", "16 Oct.", "17 Oct.", "18 Oct.", "19 Oct.",
+                "20 Oct.", "21 Oct.", "22 Oct.", "23 Oct.", "24 Oct.", "25 Oct.", "26 Oct.", "27 Oct.", "28 Oct.",
+                "29 Oct.", "30 Oct.", "31 Oct.", "1 Nov.", "2 Nov.", "3 Nov.", "4 nov."};
         xAxis.setValueFormatter(new AxisFormatter(quarters));
         xAxis.setLabelRotationAngle(90.0f);
+
 
         YAxis yAxis = chartTest.getAxisLeft();
         yAxis.setTextSize(14f); // set the text size
@@ -106,36 +117,66 @@ public class TabFragment_Chart extends Fragment {
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
         BarEntry v1e1 = new BarEntry(1.00f, 300);
         valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(2.00f, 350);
+        BarEntry v1e2 = new BarEntry(2.00f, 450);
         valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(3.00f, 498);
+        BarEntry v1e3 = new BarEntry(3.00f, 550);
         valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(4.00f, 1200);
+        BarEntry v1e4 = new BarEntry(4.00f, 900);
         valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(5.00f, 1100);
+        BarEntry v1e5 = new BarEntry(5.00f, 1200);
         valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(6.00f, 900);
+        BarEntry v1e6 = new BarEntry(6.00f, 600);
         valueSet1.add(v1e6);
-        BarEntry v1e7 = new BarEntry(7.00f, 307);
+        BarEntry v1e7 = new BarEntry(7.00f, 200);
         valueSet1.add(v1e7);
-        BarEntry v1e8 = new BarEntry(8.00f, 350);
+        BarEntry v1e8 = new BarEntry(8.00f, 103);
         valueSet1.add(v1e8);
-        BarEntry v1e9 = new BarEntry(9.00f, 790);
+        BarEntry v1e9 = new BarEntry(9.00f, 320);
         valueSet1.add(v1e9);
-        BarEntry v1e10 = new BarEntry(10.00f, 325);
+        BarEntry v1e10 = new BarEntry(10.00f, 1000);
         valueSet1.add(v1e10);
-        BarEntry v1e11 = new BarEntry(11.00f, 730);
+        BarEntry v1e11 = new BarEntry(11.00f, 850);
         valueSet1.add(v1e11);
-        BarEntry v1e12 = new BarEntry(12.00f, 298);
+        BarEntry v1e12 = new BarEntry(12.00f, 920);
         valueSet1.add(v1e12);
-        BarEntry v1e13 = new BarEntry(13.00f, 210);
+        BarEntry v1e13 = new BarEntry(13.00f, 310);
         valueSet1.add(v1e13);
-        BarEntry v1e14 = new BarEntry(14.00f, 289);
+        BarEntry v1e14 = new BarEntry(14.00f, 340);
         valueSet1.add(v1e14);
-        BarEntry v1e15 = new BarEntry(15.00f, 304);
+        BarEntry v1e15 = new BarEntry(15.00f, 399);
         valueSet1.add(v1e15);
-        BarEntry v1e16 = new BarEntry(16.00f, 317);
+        BarEntry v1e16 = new BarEntry(16.00f, 810);
         valueSet1.add(v1e16);
+        BarEntry v1e17 = new BarEntry(17.00f, 900);
+        valueSet1.add(v1e17);
+        BarEntry v1e18 = new BarEntry(18.00f, 245);
+        valueSet1.add(v1e18);
+        BarEntry v1e19 = new BarEntry(19.00f, 200);
+        valueSet1.add(v1e19);
+        BarEntry v1e20 = new BarEntry(20.00f, 450);
+        valueSet1.add(v1e20);
+        BarEntry v1e21 = new BarEntry(21.00f, 390);
+        valueSet1.add(v1e21);
+        BarEntry v1e22 = new BarEntry(22.00f, 202);
+        valueSet1.add(v1e22);
+        BarEntry v1e23 = new BarEntry(23.00f, 450);
+        valueSet1.add(v1e23);
+        BarEntry v1e24 = new BarEntry(24.00f, 1200);
+        valueSet1.add(v1e24);
+        BarEntry v1e25 = new BarEntry(25.00f, 100);
+        valueSet1.add(v1e25);
+        BarEntry v1e26 = new BarEntry(26.00f, 110);
+        valueSet1.add(v1e26);
+        BarEntry v1e27 = new BarEntry(27.00f, 260);
+        valueSet1.add(v1e27);
+        BarEntry v1e28 = new BarEntry(28.00f, 802);
+        valueSet1.add(v1e28);
+        BarEntry v1e29 = new BarEntry(29.00f, 500);
+        valueSet1.add(v1e29);
+        BarEntry v1e30 = new BarEntry(30.00f, 350);
+        valueSet1.add(v1e30);
+        BarEntry v1e31 = new BarEntry(31.00f, 320);
+        valueSet1.add(v1e31);
 
 
 
@@ -149,8 +190,6 @@ public class TabFragment_Chart extends Fragment {
             else
                 colors[i] = Color.rgb(253, 1, 1);
         }
-
-
         barDataSet1.setColors(colors);
        /* BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
         barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);*/
@@ -163,93 +202,4 @@ public class TabFragment_Chart extends Fragment {
     }
 
 
-
-
-    /*// TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public TabFragment_Chart() {
-        // Required empty public constructor
-    }
-
-    *//**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TabFragment_Chart.
-     *//*
-    // TODO: Rename and change types and number of parameters
-    public static TabFragment_Chart newInstance(String param1, String param2) {
-        TabFragment_Chart fragment = new TabFragment_Chart();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_fragment__chart, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    *//**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     *//*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
