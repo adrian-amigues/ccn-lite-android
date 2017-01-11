e475f902a39c41c6c466f682c8bb51270f9e3677
Maria <maria.rajabzadehnamaghi.5780@student.uu.se>
2017-01-11 11:26:13 +0100

    version for getting picture of fake data in the graph

--- app/src/main/java/ch/unibas/ccn_lite_android/models/Prediction.java
+++ app/src/main/java/ch/unibas/ccn_lite_android/models/Prediction.java
@@ -64,12 +64,12 @@ public class Prediction {
     }
 
     void  parseHistoricString(String histo){
-        histo = "[{'sn': 119, 'v': '2.13 ', 'u': '%', 't': 1483524866, 'n': 'Humidity'}," +
+      /*  histo = "[{'sn': 119, 'v': '2.13 ', 'u': '%', 't': 1483524866, 'n': 'Humidity'}," +
         "{'sn': 119, 'v': '28.843', 'u': 'C', 't': 1483524866, 'n': 'Temperature'}," +
                 "{'sn': 119, 'v': '253.12', 'u': 'Lux', 't': 1483524866, 'n': 'Light'}," +
                 " {'sn': 119, 'v': '2.13 ', 'u': '%', 't': 1483524866, 'n': 'Humidity'}," +
                 " {'sn': 119, 'v': '28.843', 'u': 'C', 't': 1483524866, 'n': 'Temperature'}," +
-                "{'sn': 119, 'v': '253.12', 'u': 'Lux', 't': 1483524866, 'n': 'Light'}]";
+                "{'sn': 119, 'v': '253.12', 'u': 'Lux', 't': 1483524866, 'n': 'Light'}]";*/
         if(!histo.equals("")) {
             try {
                 JSONArray jsonArray = new JSONArray(histo);
@@ -90,7 +90,9 @@ public class Prediction {
                         //historicalYValue[i]=humidtyValue;
                         historyValueArrayList.add(humidtyValue);
                         String time = object.getString("t");
-                        long t = Long.parseLong(time);
+                        Float f = new Float(Float.parseFloat(time));
+                        long t = f.longValue();
+                        //long t = Long.parseLong(time);
                         //String s = DateUtils.formatElapsedTime(t);
                         Calendar cal = Calendar.getInstance();
                         cal.setTimeInMillis(t * 1000);
@@ -151,15 +153,15 @@ public class Prediction {
         //An arrayList of old data
         ArrayList<Entry> valueSetPast = new ArrayList<>();
         float counter = 1.00f;
-       /* if(historicalYValue != null) {
+        if(historicalYValue != null) {
             for (int i = 0; i < historicalYValue.length; i++) {
                 Entry v = new Entry(counter, historicalYValue[i]);
                 counter++;
                 valueSetPast.add(v);
             }
             counter = historicalYValue.length;
-        }*/
-        Entry v1e1 = new Entry(1.00f, 3);
+        }
+        /*Entry v1e1 = new Entry(1.00f, 3);
         valueSetPast.add(v1e1);
         Entry v1e2 = new Entry(2.00f, 3);
         valueSetPast.add(v1e2);
@@ -178,21 +180,27 @@ public class Prediction {
         Entry v1e9 = new Entry(9.00f, 4);
         valueSetPast.add(v1e9);
         Entry v1e10 = new Entry(10.00f, 3.75f);
-        valueSetPast.add(v1e10);
+        valueSetPast.add(v1e10);*/
 
 
 
         //an arrayList of predicted data
         ArrayList<Entry> valueSetFuture = new ArrayList<>();
-        /*if(predictionYValue != null) {
-            for (int i = 0; i < predictionYValue.length; i++) {
-                Entry v = new Entry(counter, predictionYValue[i]);
+        Entry v;
+        if(historicalYValue.length > 0){
+            v = new Entry(counter, historicalYValue[historicalYValue.length-1]);
+            counter++;
+            valueSetFuture.add(v);
+        }
+        if(predictionYValue != null) {
+            for (int i = 1; i < predictionYValue.length; i++) {
+                v = new Entry(counter, predictionYValue[i]);
                 counter++;
                 valueSetFuture.add(v);
             }
-        }*/
+        }
 
-        Entry v1e10Join = new Entry(10.00f, 3.75f);
+        /*Entry v1e10Join = new Entry(10.00f, 3.75f);
         valueSetFuture.add(v1e10Join);
         Entry v1e11 = new Entry(11.00f, 4);
         valueSetFuture.add(v1e11);
@@ -205,10 +213,10 @@ public class Prediction {
         Entry v1e15 = new Entry(15.00f, 4);
         valueSetFuture.add(v1e15);
         Entry v1e16 = new Entry(16.00f, 3);
-        valueSetFuture.add(v1e16);
+        valueSetFuture.add(v1e16);*/
 
-        LineDataSet lineDataSetPast = new LineDataSet(valueSetPast, "Brand 1");
-        LineDataSet lineDataSetFuture = new LineDataSet(valueSetFuture, "Brand 2");
+        LineDataSet lineDataSetPast = new LineDataSet(valueSetPast, "Historical Data");
+        LineDataSet lineDataSetFuture = new LineDataSet(valueSetFuture, "Prediction Data");
 
         //Configures the line drawn for the old data
         lineDataSetPast.setColor(Color.BLUE);
@@ -238,9 +246,9 @@ public class Prediction {
         chartTest.setData(data);
 
         //Configures the description of the chart
-       /* String description = "Prediction Chart";
+        String description = "";
         chartTest.setDescription(description);
-        chartTest.setDescriptionPosition(handler.contentLeft() + 2*handler.offsetLeft() + 2*description.length(), handler.contentTop());
+       /* chartTest.setDescriptionPosition(handler.contentLeft() + 2*handler.offsetLeft() + 2*description.length(), handler.contentTop());
         chartTest.setDescriptionTextSize(14f);*/
 
         //chartTest.setFitBars(true);
@@ -264,26 +272,25 @@ public class Prediction {
         xAxis.setGranularity(1f);// minimum axis-step (interval) is 1
         xAxis.setAxisMinValue(0);
         // the labels that should be drawn on the XAxis
-        final String[] quarters2 = new String[historicalTimeValue.size()+predictionTimeValue.size()+1];
-       /* quarters[0] = "";
+        final String[] quarters = new String[historicalTimeValue.size()+predictionTimeValue.size()+1+1];
+        quarters[0] = "";
         for(int a=1;a<=historicalTimeValue.size();a++)
             quarters[a]=historicalTimeValue.get(a-1);
+
         for(int a = 0;a<predictionTimeValue.size();a++)
             quarters[a+historicalTimeValue.size()+1] = predictionTimeValue.get(a);
        // { "9.00", "10.00", "11.00", "12.00", "13.00", "14.00", "15.00", "16.00", "17.00", "18.00", "19.00", "20.00", "21.00", "22.00", "23.00", "00.00", "00.01" };
-        if(quarters.length != 0)*/
-
-        final String[] quarters = new String[] { "9.00", "10.00", "11.00", "12.00", "13.00", "14.00", "15.00", "16.00", "17.00", "18.00", "19.00", "20.00", "21.00", "22.00", "23.00", "00.00", "00.01" };
-
-        xAxis.setValueFormatter(new AxisFormatter(quarters));
+        if(quarters.length != 0)
+            xAxis.setValueFormatter(new AxisFormatter(quarters));
         xAxis.setLabelRotationAngle(90.0f);
 
         //Configures the Y Axis of the chart
         YAxis yAxis = chartTest.getAxisLeft();
         yAxis.setTextSize(14f); // set the text size
         yAxis.setTextColor(Color.BLACK);
-        yAxis.setGranularity(1f); // interval 1
+        yAxis.setGranularity(2f); // interval 1
         yAxis.setDrawGridLines(false);
+        yAxis.setAxisMaxValue(yAxis.getAxisMaximum()+3);
 
         YAxis rightAxis = chartTest.getAxisRight();
         rightAxis.setEnabled(false);
